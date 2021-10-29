3171e7d870c5f3605f4b7e6acddb12acea04ecfa
Haruki Kondo <44923695+mashharuki@users.noreply.github.com>
2021-10-29 22:36:19 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+/**
+ * メインコンポーネント
+ */
+import { useState } from "react";
+import "./styles.css";
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
+/**
+ * ログアウトボタンコンポーネント
+ */
+const Logoutbutton = (props) => (
+  <button onClick={props.toggleIsLoggedIn}>ログアウト</button>
 );
+
+/**
+ * ログインボタンコンポーネント
+ */
+const Loginbutton = (props) => (
+  <button onClick={props.toggleIsLoggedOut}>ログイン</button>
+);
+
+/**
+ * ログインの状態を管理するコンポーネント
+ */
+const LoginControl = () => {
+  // ステート変数を用意する。
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  /**
+   * クリックでステート変数を変化させる関数
+   */
+  const toggleIsLoggedIn = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
+
+  // isLoggedInの値で表示するボタンを変更する。
+  if (!isLoggedIn) {
+    return <Loginbutton toggleIsLoggedOut={toggleIsLoggedIn} />;
+  }
+
+  return <Logoutbutton toggleIsLoggedIn={toggleIsLoggedIn} />;
+};
+
+export default function App() {
+  return <LoginControl />;
+}
