f281a707134020b1c04186a168f5cd48bca73fde
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-10-30 20:59:58 +0900

    モックサーバーと通信するためのファイルを追加。

--- todo-app/src/apis/todo.js
+++ todo-app/src/apis/todo.js
@@ -1,3 +1,42 @@
 /**
  * サーバーと通信するAPIを定義したファイル
  */
+import axios from 'axios';
+// ローカルに準備したモックサーバーのURL
+const todoDataUrl = "http://localhost:3100/todos";
+
+/**
+ * GETリクエストによりToDoを取得する関数
+ */
+export const getAllTodosData = async () => {
+    // データを取得する。
+    const response = await axios.get(todoDataUrl);
+    return response.data;
+};
+
+/**
+ * POSTリクエストによりToDoを追加する関数
+ */
+export const postTodosData = async (todo) => {
+    // データを追加する。
+    const response = await axios.post(todoDataUrl, todo);
+    return response.data;
+};
+
+/**
+ * DELETEリクエストによりToDoを削除する関数
+ */
+export const deleteTodosData = async (id) => {
+    // データを削除する。
+    await axios.delete(`${todoDataUrl}/${id}`);
+    return id;
+};
+
+/**
+ * PUTリクエストによりToDoを更新する関数
+ */
+export const updataTodosData = async (id, todo) => {
+    // データを更新する。
+    const response = await axios.put(`${todoDataUrl}/${id}`, todo);
+    return response.data;
+};
