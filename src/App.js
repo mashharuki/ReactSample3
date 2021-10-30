0875cdb5fc97fdfa5a95f9b478ff130433e853c9
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-10-30 12:19:47 +0900

    memoフックをつかったサンプルアプリ実装

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import "./styles.css";
+/**
+ * メインコンポーネント
+ */
+ import React,{ memo, useState, useEffect } from "react";
+ import "./styles.css";
+ 
+ /**
+  * CountResultコンポーネント
+  */
+ const CountResult = memo(({text, countState}) => {
+   console.log(`${text}ボタンがクリックされました。`);
+ 
+   return (
+     <p>
+       {text} : {countState}
+     </p>
+   );
+ });
+ 
+ /**
+  * Counterコンポーネント
+  */
+ const Counter = () => {
+   // ステート変数
+   const [countStateA, setCountStateA] = useState(0);
+   const [countStateB, setCountStateB] = useState(0);
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+   /**
+    * カウントアップする関数
+    */
+   const countIncrementA = () => {
+    setCountStateA((prevCountStateA) => prevCountStateA + 1);
+   };
+
+   const countIncrementB = () => {
+    setCountStateB((prevCountStateB) => prevCountStateB + 1);
+   };
+ 
+   return (
+     <>
+       <CountResult text="Aボタン" countState={countStateA} />
+       <CountResult text="Bボタン" countState={countStateB} />
+       <button onClick={countIncrementA}>Aボタン</button>
+       <button onClick={countIncrementB}>Bボタン</button>
+     </>
+   );
+ }
+ 
+ export default function App() {
+   return <Counter />;
+ };
+ 
--- src/index.js
+++ src/index.js
@@ -1,81 +1,13 @@
-/**
- * メインコンポーネント
- */
-import React,{ useState, useEffect } from "react";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-// 初期値
-const INITIAL_COUNT = 0;
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    ), document.getElementById('root')
+);
 
-/**
- * タイマーコンポーネント
- */
-const Timer = () => {
-  // ステート変数
-  const [ count, setCount ] = useState(INITIAL_COUNT);
-
-  /**
-   * 副作用フックの宣言
-   */
-  useEffect(callbackFunction, [count]);
-
-  /**
-   * 副作用関数を宣言する。
-   */
-  const callbackFunction = () => {
-    alert("副作用関数が実行されました。");
-    // タイマー設定
-    const timer = setInterval(countIncrement, 1000);
-    // レンダリングする内容
-    return () => {
-      console.log("timerが削除されました。");
-      // timerを削除
-      clearInterval(timer);
-    };
-  };
-
-  /**
-   * カウントアップする関数
-   */  
-  const countIncrement = () => {
-    setCount((prevCount) => prevCount + 1);
-    console.log("カウントアップ + 1");
-  };
-
-  /**
-   * カウントをリセットするための関数
-   */
-  const countReset = () => {
-    setCount(INITIAL_COUNT);
-  };
-
-  // レンダリングする内容
-  return (
-    <div className="App">
-      <p>現在のカウント数：{count}</p>
-      <button onClick={countReset}>リセット</button>
-    </div>
-  );
-}
-
-export default function App() {
-  // ステート変数
-  const [ display, toggleDisplay ] = useState(false);
-
-  /**
-   * displayの状態をトグルする関数
-   */
-  const handleToggleDisplay = () => {
-    toggleDisplay(!display);
-  };
-
-  return (
-    <>
-      <button onClick={handleToggleDisplay}>
-        {display ? "タイマーを非表示" : "タイマーを表示"}
-      </button>
-
-      {display && <Timer/>}
-    </>
-  );
-}
