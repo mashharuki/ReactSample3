9b028510e9116e4a818a526c3a9f1648b214d3c0
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-10-30 14:59:26 +0900

    useRefを使ったサンプル機能を実装する。

--- src/App.js
+++ src/App.js
@@ -1,58 +1,52 @@
 /**
  * メインコンポーネント
  */
- import React,{ memo, useState, useEffect, useMemo } from "react";
- import "./styles.css";
+import React,{ useRef, useState, useEffect } from "react";
+import "./styles.css";
  
- /**
-  * 長方形の面積を求めるsquareコンポーネント
-  */
-const square = (params) => {
-  // 配列を生成
-  const testArea = [...Array(1000).keys()];
-  // 重い処理(テスト用)
-  testArea.forEach(() => {
-    console.log(`「計算B+1」がボタンクリックされ、squre関数実行中, ループ処理を${testArea.length}回実行中`);
-  });
-  // 面積を計算
-  return params * params;
-};
- 
- /**
-  * Counterコンポーネント
-  */
-const Counter = () => {
+/**
+ * Sampleコンポーネント
+ */
+const SampleComponent = () => {
    // ステート変数
-   const [countStateA, setCountStateA] = useState(0);
-   const [countStateB, setCountStateB] = useState(0);
+   const [ text, setText ] = useState("");
+   const inputRefObject = useRef(null);
 
    /**
-    * カウントアップする関数
+    * 副作用関数
     */
-   const countResultA = () => {
-    setCountStateA((prevCountA) => prevCountA + 1);
-    console.log("計算：A+1ボタンがクリックされました。");
-   };
+  useEffect(() => {
+    console.log("レンダリング！");
+  });
 
-   const countResultB = () => {
-    setCountStateB((prevCountB) => prevCountB + 1);
-    console.log("計算：B+1ボタンがクリックされました。");
-   };
- 
-   // 面積を求める。
-   const squareArea = useMemo(() => square(countStateB), [countStateB]);
 
-   return (
-     <>
-       <p><button onClick={countResultA}>計算：A + 1</button></p>
-       <p><button onClick={countResultB}>計算：B + 1</button></p>
-       <p>【正方形の面積】</p>
-       <p>計算結果B  * 計算結果B = {squareArea}</p>
-     </>
-   );
- }
+
+   /**
+    * クリックした時の関数
+    */
+  const handleClick = () => {
+    setText(inputRefObject.current.value);
+  };
+
+  /**
+   * 初期値を設定する関数
+   */
+  const textReset = () => {
+    setText("");
+    inputRefObject.current.value = "";
+  }
+
+  return (
+    <>
+      <input ref={inputRefObject} type="text" />
+      <button onclick={handleClick}>set text</button>
+      <button onClick={textReset}>reset</button>
+      <p>text: {text}</p>
+    </>
+  );
+}
  
  export default function App() {
-   return <Counter />;
+   return <SampleComponent />;
  };
  
